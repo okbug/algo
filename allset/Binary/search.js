1abd7d41706f8d6a0b607e65530112e73bda5663
okbug <1583978087@qq.com>
2021-01-23 19:56:01 +0800

    1

---
+++ allset/Binary/search.js
@@ -0,0 +1,14 @@
+function search(nums, target) {
+  let left = 0, right = nums.length - 1
+  while(left <= right) {
+    let mid = left + (right - left) << 1
+    if(nums[mid] < target) {
+      left = mid + 1
+    } else if (nums[mid] > target) {
+      right = mid - 1
+    } else {
+      return mid
+    }
+  } 
+  return -1
+}
