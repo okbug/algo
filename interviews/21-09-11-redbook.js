425fe787c19cfe461bb15d5cb332b578a2e7be16
okbug <1583978087@qq.com>
2021-09-12 23:46:25 +0800

    useState简版实现以及注释解析

--- interviews/21-09-11-redbook.js
+++ interviews/21-09-11-redbook.js
@@ -80,3 +80,32 @@ function useState(initialState) {
     index++;
     return [state, setState];
 }
+
+
+
+// updated
+
+// useState新的实现
+
+let hookState = [];
+let hookIndex = 0;
+
+function useState(initialState) {
+    hookState[hookIndex] = hookState[hookIndex] || initialState;
+    let currentIndex = hookIndex;
+    function setState(newState) {
+        hookState[currentIndex] = newState;
+        hookIndex = 0;
+    }
+
+    return [hookState[hookIndex++], setState];
+}
+
+/**
+ * 第一步： 先将数组中当前下标的存储，如果是第一次执行， 走 || 后面，然后都是走前面的了
+ * 第二步： 保存当前的下标索引
+ * 第三步： 写setState函数，如果这个函数被执行了的话，会更新，此时所有的组件要重新执行
+ * 那么所有的useState都会重新执行
+ * 所以索引要从0开始，所以在更新函数中将全局的索引设置为0
+ * 第四步： 在最后返回的时候将索引+1 这样下一次调用useState的时候就是取得数组之中下一个值
+ */
